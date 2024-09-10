b559bcab8058a027b2554ed1496aaf4ade729e42
Ronald Ogando <ronaldogando150@gmail.com>
2024-09-10 17:52:20 -0400

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AppConfigService } from './config';
+import { DriversService } from './modules/drivers/service/drivers.service';
+import { DriversController } from './modules/drivers/controller/drivers.controller';
+import { PassengersService } from './modules/passengers/service/passengers.service';
+import { PassengersController } from './modules/passengers/controller/passengers.controller';
+import { TripsService } from './modules/trips/service/trips.service';
+import { TripsController } from './modules/trips/controller/trips.controller';
+import { InvoicesService } from './modules/invoices/service/invoices.service';
+import { InvoicesController } from './modules/invoices/controller/invoices.controller';
 
 @Module({
   imports: [
@@ -10,8 +18,21 @@ import { AppConfigService } from './config';
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService, AppConfigService],
+  controllers: [
+    AppController,
+    DriversController,
+    PassengersController,
+    TripsController,
+    InvoicesController,
+  ],
+  providers: [
+    AppService,
+    AppConfigService,
+    DriversService,
+    PassengersService,
+    TripsService,
+    InvoicesService,
+  ],
   exports: [AppConfigService],
 })
 export class AppModule {}
