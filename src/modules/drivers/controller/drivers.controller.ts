4b62830b2d32353b60543f9d64c3ddb91b5558f9
Ronald Ogando <ronaldogando150@gmail.com>
2024-09-10 18:50:29 -0400

    add endpoint for get frivers

--- src/modules/drivers/controller/drivers.controller.ts
+++ src/modules/drivers/controller/drivers.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { DriversService } from '../service/drivers.service';
+import { Driver } from '../entities';
+import { PaginationResponseDto } from 'src/common';
 
 @Controller('drivers')
-export class DriversController {}
+export class DriversController {
+  constructor(private readonly driversService: DriversService) {}
+
+  @Get()
+  async findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ): Promise<PaginationResponseDto<Driver>> {
+    //TODO: Add a constant limit for eveything
+    return this.driversService.findAll(page, limit);
+  }
+}
--- src/modules/drivers/service/drivers.service.ts
+++ src/modules/drivers/service/drivers.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationResponseDto } from 'src/common';
+import { Repository } from 'typeorm';
+import { Driver } from '../entities/driver.entity';
 
 @Injectable()
-export class DriversService {}
+export class DriversService {
+  constructor(
+    @InjectRepository(Driver)
+    private readonly driversRepository: Repository<Driver>,
+  ) {}
+
+  async findAll(
+    page: number,
+    limit: number,
+  ): Promise<PaginationResponseDto<Driver>> {
+    const [data, total] = await this.driversRepository.findAndCount({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+
+    const basePath = 'drivers';
+    return new PaginationResponseDto(data, total, page, limit, basePath);
+  }
+}
