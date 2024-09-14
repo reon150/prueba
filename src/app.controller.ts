c3aea1b30cebdc3ccd3a7d8d94c72ca4f36264d9
Ronald Ogando <ronaldogando150@gmail.com>
2024-09-13 20:38:31 -0400

    add doc for health check status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Check health status of the service',
+    description:
+      'This endpoint is used to confirm the service is operational and returns a specific greeting message as a sign of life.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Service is up and running',
+    schema: {
+      example: 'Hello World!',
+    },
+  })
   getHello(): string {
     return this.appService.getHello();
   }
