fab168bec605cc61e2506f771b0f82a1174830f6
Ronald Ogando <ronaldogando150@gmail.com>
2024-09-15 15:02:46 -0400

    change endpoints order in driver controller

--- src/modules/drivers/controller/drivers.controller.ts
+++ src/modules/drivers/controller/drivers.controller.ts
@@ -18,37 +18,13 @@ import {
   ApiBadRequestResponse,
   ApiPaginatedResponse,
   PaginationResponseDto,
+  UUIDValidationPipe,
 } from '../../../common';
 
 @ApiTags('drivers')
 @Controller('drivers')
 export class DriversController {
   constructor(private readonly driversService: DriversService) {}
-
-  @Get(':id')
-  @ApiOperation({
-    summary: 'Get a specific driver',
-    description:
-      'Retrieves detailed information about a specific driver, including all associated trips.',
-  })
-  @ApiNotFoundResponse({ description: 'Driver not found' })
-  async findOne(@Param('id') id: string): Promise<GetDriverResponseDto> {
-    return this.driversService.findOne(id);
-  }
-
-  @Get()
-  @ApiOperation({
-    summary: 'Retrieve a list of drivers',
-    description:
-      'Returns a paginated list of drivers, optionally filtered by their availability.',
-  })
-  @ApiPaginatedResponse(GetDriversResponseDto)
-  @ApiBadRequestResponse()
-  async findAll(
-    @Query() query: GetDriversRequestDto,
-  ): Promise<PaginationResponseDto<GetDriversResponseDto>> {
-    return this.driversService.findAll(query);
-  }
 
   @Get('/nearby')
   @ApiOperation({
@@ -67,5 +43,32 @@ export class DriversController {
     @Query() query: GetNearbyDriversRequestDto,
   ): Promise<GetNearbyDriversResponseDto[]> {
     return this.driversService.findNearby(query);
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Get a specific driver',
+    description:
+      'Retrieves detailed information about a specific driver, including all associated trips.',
+  })
+  @ApiNotFoundResponse({ description: 'Driver not found' })
+  async findOne(
+    @Param('id', UUIDValidationPipe) id: string,
+  ): Promise<GetDriverResponseDto> {
+    return this.driversService.findOne(id);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: 'Retrieve a list of drivers',
+    description:
+      'Returns a paginated list of drivers, optionally filtered by their availability.',
+  })
+  @ApiPaginatedResponse(GetDriversResponseDto)
+  @ApiBadRequestResponse()
+  async findAll(
+    @Query() query: GetDriversRequestDto,
+  ): Promise<PaginationResponseDto<GetDriversResponseDto>> {
+    return this.driversService.findAll(query);
   }
 }
