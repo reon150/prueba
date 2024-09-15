b659b470ce536e09b984a65e4bcc926f3a3bb8aa
Ronald Ogando <ronaldogando150@gmail.com>
2024-09-15 00:54:24 -0400

    add tets for create in trips service

--- src/modules/trips/service/trips.service.spec.ts
+++ src/modules/trips/service/trips.service.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TripsService } from './trips.service';
 import { InvoicesService } from '../../invoices/service/Invoices.service';
-import { InvoiceToDtoMapper } from '../mappers';
-import { GetInvoiceResponseDto } from '../dto';
-import { NotFoundException } from '@nestjs/common';
+import { InvoiceToDtoMapper, TripToDtoMapper } from '../mappers';
+import {
+  CreateTripRequestDto,
+  CreateTripResponseDto,
+  GetInvoiceResponseDto,
+} from '../dto';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Trip } from '../entities';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
@@ -11,6 +15,7 @@ import { repositoryMock } from '../../../common';
 import { Invoice } from '../../invoices/entities';
 import { PassengersService } from '../../passengers/service/passengers.service';
 import { DriversService } from '../../drivers/service/drivers.service';
+import { TripStatus } from '../enums';
 
 jest.mock('../mappers/invoice-to-dto.mapper');
 
@@ -101,6 +106,82 @@ describe('TripsService', () => {
 
       await expect(service.findInvoiceByTripId(tripId)).rejects.toThrow(
         NotFoundException,
+      );
+    });
+  });
+
+  describe('create', () => {
+    const createTripRequestDtoMock: CreateTripRequestDto = {
+      driverId: '09385a54-5cc5-48dc-aaa1-4aae0c4de4f9',
+      passengerId: 'fff263cc-c5d8-455b-9ba9-e3d9e9938916',
+      startLatitude: 34.0522,
+      startLongitude: -118.2437,
+      startTime: new Date(),
+    };
+
+    it('should successfully create a trip', async () => {
+      const createTripResponseDto = new CreateTripResponseDto();
+      const tripMock = new Trip();
+      const tripSavedMock = new Trip();
+      tripSavedMock.id = '2d804f90-0149-4df6-a0b9-5333cd9e2cc7';
+
+      jest.spyOn(driversService, 'driverExists').mockResolvedValue(true);
+      jest.spyOn(driversService, 'isDriverAvailable').mockResolvedValue(true);
+      jest.spyOn(passengersService, 'passengerExists').mockResolvedValue(true);
+      jest.spyOn(tripsRepository, 'create').mockReturnValue(tripMock);
+      jest.spyOn(tripsRepository, 'save').mockResolvedValue(tripSavedMock);
+      jest
+        .spyOn(TripToDtoMapper, 'toCreateTripResponseDto')
+        .mockReturnValue(createTripResponseDto);
+
+      const result = await service.create(createTripRequestDtoMock);
+
+      expect(driversService.driverExists).toHaveBeenCalledWith(
+        createTripRequestDtoMock.driverId,
+      );
+      expect(driversService.isDriverAvailable).toHaveBeenCalledWith(
+        createTripRequestDtoMock.driverId,
+      );
+      expect(passengersService.passengerExists).toHaveBeenCalledWith(
+        createTripRequestDtoMock.passengerId,
+      );
+      expect(tripsRepository.create).toHaveBeenCalledWith(
+        createTripRequestDtoMock,
+      );
+      expect(tripsRepository.save).toHaveBeenCalledWith(
+        expect.objectContaining({
+          status: TripStatus.Active,
+        }),
+      );
+      expect(result).toEqual(createTripResponseDto);
+    });
+
+    it('should throw NotFoundException if driver does not exist', async () => {
+      jest.spyOn(driversService, 'driverExists').mockResolvedValue(false);
+      jest.spyOn(passengersService, 'passengerExists').mockResolvedValue(true);
+
+      await expect(service.create(createTripRequestDtoMock)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    it('should throw NotFoundException if passenger does not exist', async () => {
+      jest.spyOn(driversService, 'driverExists').mockResolvedValue(true);
+      jest.spyOn(driversService, 'isDriverAvailable').mockResolvedValue(true);
+      jest.spyOn(passengersService, 'passengerExists').mockResolvedValue(false);
+
+      await expect(service.create(createTripRequestDtoMock)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    it('should throw BadRequestException if driver is not available', async () => {
+      jest.spyOn(driversService, 'driverExists').mockResolvedValue(true);
+      jest.spyOn(driversService, 'isDriverAvailable').mockResolvedValue(false);
+      jest.spyOn(passengersService, 'passengerExists').mockResolvedValue(true);
+
+      await expect(service.create(createTripRequestDtoMock)).rejects.toThrow(
+        BadRequestException,
       );
     });
   });
